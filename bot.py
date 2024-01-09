3903f03ae81a35211011d095fe4e2cc8ba9f5835
Mohitag403 <149030402+Mohitag403@users.noreply.github.com>
2024-01-09 20:56:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -43,9 +43,9 @@ logging.basicConfig(
 
 bot = Client(
     "bot",
-    api_id= 22779671,
-    api_hash= "125d8d88b77309dc3b154cbbfc2dacb2",    
-    bot_token= "6847175705:AAHbkU8GFmzoxR9dCQTr6RuZ4NQsev5ufz0"
+    api_id= 18618422,
+    api_hash= "f165b1caec3cfa4df943fe1cbe82d22a",    
+    bot_token= "6331627404:AAHgjoOQqF4ClxNekEhFibzRVSslt_tsfsk"
 )
 
 @bot.on_message(filters.command(["cp"]))
